6bdb1c092def5a51bdc52e6c179c01fa8b5d11d0
B303-xx <administrador@b303-21.local>
2016-10-27 08:38:26 -0500

    películasDALC add

---
+++ PeliculasCJ/PeliculasCJ/DALC/PeliculasDALC.swift
@@ -0,0 +1,13 @@
+//
+//  PeliculasDALC.swift
+//  PeliculasCJ
+//
+//  Created by B303-21 on 27/10/16.
+//  Copyright © 2016 B303-20. All rights reserved.
+//
+
+import UIKit
+
+class PeliculasDALC: NSObject {
+
+}
