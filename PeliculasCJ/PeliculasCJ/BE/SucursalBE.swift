b9b5d3c564c5e6d976692fb0038188eabd1bf5e4
B303-xx <administrador@b303-20.local>
2016-10-27 08:16:09 -0500

    Creando BE

---
+++ PeliculasCJ/PeliculasCJ/BE/SucursalBE.swift
@@ -0,0 +1,13 @@
+//
+//  SucursalBE.swift
+//  PeliculasCJ
+//
+//  Created by B303-20 on 27/10/16.
+//  Copyright © 2016 B303-20. All rights reserved.
+//
+
+import UIKit
+
+class SucursalBE: NSObject {
+
+}
---
+++ PeliculasCJ/PeliculasCJ/IPhone/Clases/PeliculasTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeliculasTableViewCell.swift
+//  PeliculasCJ
+//
+//  Created by B303-20 on 27/10/16.
+//  Copyright © 2016 B303-20. All rights reserved.
+//
+
+import UIKit
+
+class PeliculasTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
