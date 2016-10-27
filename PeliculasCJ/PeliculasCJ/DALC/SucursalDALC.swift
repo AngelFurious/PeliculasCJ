ea1e48ad4a5635c0488c6a19eb73d2fb152ae943
B303-xx <administrador@b303-21.local>
2016-10-27 08:40:53 -0500

    enviar

---
+++ PeliculasCJ/PeliculasCJ/DALC/SucursalDALC.swift
@@ -0,0 +1,13 @@
+//
+//  SucursalDALC.swift
+//  PeliculasCJ
+//
+//  Created by B303-21 on 27/10/16.
+//  Copyright © 2016 B303-20. All rights reserved.
+//
+
+import UIKit
+
+class SucursalDALC: NSObject {
+
+}
